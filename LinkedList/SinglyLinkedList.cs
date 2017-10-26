14e55e05ba226823b868e543948af95169020b4d
Bali Vaibhav <bail.vaibhav@recreo.com.au>
2017-10-27 10:45:46 +1100

    Implemented SinglyLinkedList

--- LinkedList/SinglyLinkedList.cs
+++ LinkedList/SinglyLinkedList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -6,10 +7,17 @@ namespace LinkedList
 {
     public class SinglyLinkedList<T> : ICollection<T>
     {
+        private LinkedListNode<T> Head { get; set; }
+        private LinkedListNode<T> Tail { get; set; }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var current = Head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -19,30 +27,159 @@ namespace LinkedList
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            AddToFront(item);
         }
 
+        /// <summary>
+        /// Adds item to the front of the Linked List
+        /// </summary>
+        /// <param name="item"></param>
+        public void AddToFront(T item)
+        {
+            var node = new LinkedListNode<T>(item);
+            if (Head == null)
+            {
+                Head = Tail = node;
+            }
+            else
+            {
+                var temp = Head;
+                Head = node;
+                Head.Next = temp;
+            }
+
+            ++Count;
+        }
+
+        /// <summary>
+        /// Method to add a new item to the back of the list
+        /// </summary>
+        /// <param name="item"></param>
+        public void AddToBack(T item)
+        {
+            if (Head == null)
+            {
+                Add(item);
+                return;
+            }
+
+            var temp = Tail;
+            Tail = new LinkedListNode<T>(item);
+            temp.Next = Tail;
+            ++Count;
+        }
+
+        /// <summary>
+        /// Clears the Linked List
+        /// </summary>
         public void Clear()
         {
-            throw new NotImplementedException();
+            Head = Tail = null;
+            Count = 0;
         }
 
+        /// <summary>
+        /// Tells if an item is present in the list
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            var current = Head;
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    return true;
+                }
+
+                current = current.Next;
+            }
+
+            return false;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
+            var current = Head;
+            while (current != null)
+            {
+                array[arrayIndex] = current.Value;
+                current = current.Next;
+                ++arrayIndex;
+            }
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            if (Head.Value.Equals(item))
+            {
+                RemoveFront();
+                return true;
+            }
+
+            if (Tail.Value.Equals(item))
+            {
+                RemoveBack();
+                return true;
+            }
+
+            //Find if item is in the middle
+            var current = Head; 
+            if (current.Next.Value.Equals(item)) //start at next of Head as head is not the value! previously compared
+            {
+                var temp = current.Next.Next; // 3 -> 4 -> 5 -- remove 4 current is 3 and current.next.next is going to be tail
+                current.Next = null;
+                current.Next = temp;
+                --Count;
+                return true;
+            }
+
+            return false;
         }
 
-        public int Count { get; }
-        public bool IsReadOnly { get; }
+        public void RemoveFront()
+        {
+            if (Head == null)
+            {
+                throw new InvalidOperationException("Can't call remove on empty linked list");
+            }
+
+            var temp = Head;
+            Head = null;
+            Head = temp.Next;
+            --Count;
+        }
+
+        /// <summary>
+        /// Will have to treverse
+        /// </summary>
+        public void RemoveBack()
+        {
+            if (Tail == null)
+            {
+                throw new InvalidOperationException("Can't call remove on empty linked list");
+            }
+
+            //Check if Head and tail are equal
+            if (Head == Tail)
+            {
+                Clear();
+            }
+
+            var current = Head;
+            while (current.Next != Tail)
+            {
+                current = current.Next;
+            }
+
+            current.Next = null;
+            Tail = null;
+            Tail = current;
+            --Count;
+        }
+
+        public int Count { get; private set; }
+        public bool IsReadOnly => false;
     }
 }
