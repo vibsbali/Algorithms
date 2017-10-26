9f96e34f850b160f11b11f6a0ff3ee42e63e4574
Bali Vaibhav <bail.vaibhav@recreo.com.au>
2017-10-26 16:35:12 +1100

    Added new SinglyLinkedList Project

---
+++ LinkedList/LinkedListNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace LinkedList
+{
+    public class LinkedListNode<T>
+    {
+        public LinkedListNode(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; private set; }
+        public LinkedListNode<T> Next { get; set; }
+    }
+}
---
+++ LinkedList/SinglyLinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    public class SinglyLinkedList<T> : ICollection<T>
+    {
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count { get; }
+        public bool IsReadOnly { get; }
+    }
+}
