ee28b3d89a4f59178323dbf7785d403da669a1b0
Bali Vaibhav <bail.vaibhav@recreo.com.au>
2017-10-27 11:54:00 +1100

    Wrote a test console runner for testing

---
+++ ConsoleRunner/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinkedList;
+
+namespace ConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myLinkedList = new SinglyLinkedList<int>();
+
+            myLinkedList.Add(1);
+            myLinkedList.AddToBack(2);
+            myLinkedList.AddToBack(3);
+
+
+            PrintCountAndItems(myLinkedList);
+
+            myLinkedList.Remove(2);
+            PrintCountAndItems(myLinkedList);
+
+            myLinkedList.Remove(3);
+            PrintCountAndItems(myLinkedList);
+
+            myLinkedList.Remove(1);
+            PrintCountAndItems(myLinkedList);
+
+        }
+
+        private static void PrintCountAndItems(SinglyLinkedList<int> myLinkedList)
+        {
+            Console.WriteLine($"Number of items are {myLinkedList.Count}");
+
+            foreach (var item in myLinkedList)
+            {
+                Console.WriteLine($"The item is {item}");
+            }
+        }
+    }
+}
